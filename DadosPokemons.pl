324dfc081d4cc500de9fd6b502088c45647c521d
Pedro <pedrohda.ph@gmail.com>
2023-03-27 22:12:06 -0300

    (feat): banco de dados de pokemons(sem os status)

---
+++ DadosPokemons.pl
@@ -0,0 +1,252 @@
+pokemon(bulbasaur, grama_venenoso, [ivysaur, venusaur], [45, 49, 49, 65, 65, 45]).
+pokemon(ivysaur, grama_venenoso, [venusaur], [160, 110, 65, 65, 110, 30]).
+pokemon(venusaur, grama_venenoso, [], [160, 110, 65, 65, 110, 30]).
+
+pokemon(charmander, fogo, [charmeleon, charizard], [39, 52, 43, 60, 50, 65]).
+pokemon(charmeleon, fogo, [charizard], [39, 52, 43, 60, 50, 65]).
+pokemon(charizard, fogo_voador, [], [39, 52, 43, 60, 50, 65]).
+
+pokemon(squirtle, agua, [wartortle, blastoise], [44, 48, 65, 50, 64, 43]).
+pokemon(wartortle, agua, [blastoise], [44, 48, 65, 50, 64, 43]).
+pokemon(blastoise, agua, [], [44, 48, 65, 50, 64, 43]).
+
+pokemon(caterpie, inseto,[metapod, butterfree],[]).
+pokemon(metapod, inseto,[butterfree],[]).
+pokemon(butterfree, inseto_voador,[],[]).
+
+pokemon(weedle,inseto_venenoso,[kakuna,beedrill],[]).
+pokemon(kakuna,inseto_venenoso,[beedrill],[]).
+pokemon(beedrill,inseto_venenoso,[],[]).
+
+pokemon(pidgey,normal_voador,[pidgeotto, pidgeot],[]).
+pokemon(pidgeotto,normal_voador,[pidgeot],[]).
+pokemon(pidgeot,normal_voador,[],[]).
+
+pokemon(ratata,normal,[raticate],[]).
+pokemon(raticate,normal,[],[]).
+
+pokemon(spearow,normal_voador,[fearow],[]).
+pokemon(fearow,normal_voador,[],[]).
+
+pokemon(ekans,venenoso,[arbok],[]).
+pokemon(arbok,venenoso,[],[]).
+
+pokemon(pikachu,eletrico,[raichu],[]).
+pokemon(raichu,eletrico,[],[]).
+
+pokemon(sandshrew,terrestre,[sandslash],[]).
+pokemon(sandslash,terrestre,[],[]).
+
+pokemon(nidoran,venenoso,[nidorina, nidoqueen],[]).
+pokemon(nidorina,venenoso,[nidoqueen],[]).
+pokemon(nidoqueen,venenoso_terrestre,[],[]).
+pokemon(nidoran,venenoso,[nidorina, nidoking],[]).
+pokemon(nidorino,venenoso,[nidoking],[]).
+pokemon(nidoking,venenoso_terrestre,[],[]).
+
+pokemon(clefairy,fada,[clefable],[]).
+pokemon(clefable,fada,[],[]).
+
+pokemon(vulpix,fogo,[ninetales],[]).
+pokemon(ninetales,fogo,[],[]).
+
+pokemon(jigglypuff,normal_fada,[wigglytuff],[]).
+pokemon(wigglytuff,normal_fada,[],[]).
+
+pokemon(zubat,venenoso_voador,[golbat],[]).
+pokemon(golbat,venenoso_voador,[],[]).
+
+pokemon(oddish,grama_venenoso,[gloom, vileplume],[]).
+pokemon(gloom,grama_venenoso,[vileplume],[]).
+pokemon(vileplume,grama_venenoso,[],[]).
+
+pokemon(paras,inseto_grama,[parasect],[]).
+pokemon(parasect,inseto_grama,[],[]).
+
+pokemon(venonat,inseto_venenoso,[venomoth],[]).
+pokemon(venomoth,inseto_venenoso,[],[]).
+
+pokemon(diglett,terrestre,[dugtrio],[]).
+pokemon(dugtrio,terrestre,[],[]).
+
+pokemon(meowth,normal,[persian],[]).
+pokemon(persian,normal,[],[]).
+
+pokemon(psyduck,agua,[golduck],[]).
+pokemon(golduck,agua,[],[]).
+
+pokemon(mankey,lutador,[primeape],[]).
+pokemon(primeape,lutador,[],[]).
+
+pokemon(growlithe,fogo,[arcanine],[]).
+pokemon(arcanine,fogo,[],[]).
+
+pokemon(poliwag,agua,[poliwhirl, poliwrath],[]).
+pokemon(poliwhirl,agua,[poliwrath],[]).
+pokemon(poliwrath,agua,[],[]).
+
+pokemon(abra,psiquico,[kadabra, alakazam],[]).
+pokemon(kadabra,psiquico,[alakazam],[]).
+pokemon(alakazam,psiquico,[],[]).
+
+pokemon(machop,lutador,[machoke, machamp],[]).
+pokemon(machoke,lutador,[machamp],[]).
+pokemon(machamp,lutador,[],[]).
+
+pokemon(bellsprout,grama_venenoso,[weepinbell, victreebel],[]).
+pokemon(weepinbell,grama_venenoso,[victreebel],[]).
+pokemon(victreebel,grama_venenoso,[],[]).
+
+pokemon(tentacool,agua_venenoso,[tentacruel],[]).
+pokemon(tentacruel,agua_venenoso,[],[]).
+
+pokemon(geodude,pedra_terrestre,[graveler, golem],[]).
+pokemon(graveler,pedra_terrestre,[golem],[]).
+pokemon(golem,pedra_terrestre,[],[]).
+
+pokemon(ponyta,fogo,[rapidash],[]).
+pokemon(rapidash,fogo,[],[]).
+
+pokemon(slowpoke,agua_psiquico,[slowbro],[]).
+pokemon(slowbro,agua_psiquico,[],[]).
+
+pokemon(magnemite,eletrico_aco,[magneton],[]).
+pokemon(magneton,eletrico_aco,[],[]).
+
+pokemon(farfetchd,normal_voador,[],[]).
+
+pokemon(doduo,normal_voador,[dodrio],[]).
+pokemon(dodrio,normal_voador,[],[]).
+
+pokemon(seel,agua,[dewgong],[]).
+pokemon(dewgong,agua,[],[]).
+
+pokemon(grimer,venenoso,[muk],[]).
+pokemon(muk,venenoso,[],[]).
+
+pokemon(shellder,agua,[cloyster],[]).
+pokemon(cloyster,agua_gelo,[],[]).
+
+pokemon(gastly,fantasma_venenoso,[haunter, gengar],[]).
+pokemon(haunter, fantasma_venenoso,[gengar],[]).
+pokemon(gengar,fantasma_venenoso,[],[]).
+
+pokemon(onix,pedra_terrestre,[],[]).
+
+pokemon(drowzee,psiquico,[hypno],[]).
+pokemon(hypno,psiquico,[],[]).
+
+pokemon(krabby,agua,[kingler],[]).
+pokemon(kingler,agua,[],[]).
+
+pokemon(voltorb,eletrico,[electrode],[]).
+pokemon(electrode,eletrico,[],[]).
+
+pokemon(exeggcute,grama_psiquico,[exeggutor],[]).
+pokemon(exeggutor,grama_psiquico,[],[]).
+
+pokemon(cubone,terrestre,[marowak],[]).
+pokemon(marowak,terrestre,[],[]).
+
+pokemon(hitmonlee,lutador,[],[]).
+pokemon(hitmonchan,lutador,[],[]).
+
+pokemon(lickitung,normal,[],[]).
+
+pokemon(coffing,venenoso,[weezing],[]).
+pokemon(weezing,venenoso,[],[]).
+
+pokemon(rhyhorn,pedra_terrestre,[rhydon],[]).
+pokemon(rhydon,pedra_terrestre,[],[]).
+
+pokemon(chansey, normal,[],[]).
+pokemon(tangela,grama,[],[]).
+pokemon(kangaskhan,normal,[],[]).
+
+pokemon(horsea,agua,[seadra],[]).
+pokemon(seadra,agua,[],[]).
+
+pokemon(goldean,agua,[seaking],[]).
+pokemon(seaking,agua,[],[]).
+
+pokemon(staryu,agua,[starmie],[]).
+pokemon(starmie,agua,[],[]).
+
+pokemon(mr_mime,psiquico_fada,[],[]).
+
+pokemon(scyther,inseto_voador,[],[]).
+pokemon(jynx,gelo_psiquico,[],[]).
+
+pokemon(electabuzz,eletrico,[],[]).
+pokemon(magmar,fogo,[],[]).
+
+pokemon(pinsir,inseto,[],[]).
+
+pokemon(tauros,normal,[],[]).
+
+pokemon(magikarp,agua,[gyarados],[]).
+pokemon(gyarados,agua_voador,[],[]).
+
+pokemon(lapras,agua_gelo,[],[]).
+pokemon(ditto,normal,[],[]).
+
+pokemon(eevee,normal,[vaporeon, jolteon, flareon],[]).
+pokemon(vaporeon,agua,[],[]).
+pokemon(jolteon,eletrico,[],[]).
+pokemon(flareon,fogo,[],[]).
+
+pokemon(porygon,normal,[],[]).
+
+pokemon(omanyte,pedra_agua,[omastar],[]).
+pokemon(omastar,pedra_agua,[],[]).
+
+pokemon(kabuto,pedra_agua,[kabutops],[]).
+pokemon(kabutops,pedra_agua,[],[]).
+
+pokemon(aerodactyl,pedra_voador,[],[]).
+
+pokemon(snorlax,normal,[],[]).
+
+pokemon(articuno,gelo_voador,[],[]).
+pokemon(zapdos,eletrico_voador,[],[]).
+pokemon(moltres,fogo_voador,[],[]).
+
+pokemon(dratini,dragon,[dragonair, dragonite],[]).
+pokemon(dragonair,dragon,[dragonite],[]).
+pokemon(dragonite,dragon_voador,[],[]).
+
+pokemon(mewtwo,psiquico,[],[]).
+pokemon(mew,psiquico,[],[]).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
