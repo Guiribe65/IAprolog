b3a73956cedc221410f212db88ce04a15c9c7955
Pedro <pedrohda.ph@gmail.com>
2023-03-28 20:23:51 -0300

    (feat): FINALMENTE ACABEI SAPOHA... TODOS OS STATUS DOS POKEMONS CARALHO

--- DadosPokemons.pl
+++ DadosPokemons.pl
@@ -1,222 +1,222 @@
 pokemon(bulbasaur, grama_venenoso, [ivysaur, venusaur], [45, 49, 49, 65, 65, 45]).
-pokemon(ivysaur, grama_venenoso, [venusaur], [160, 110, 65, 65, 110, 30]).
-pokemon(venusaur, grama_venenoso, [], [160, 110, 65, 65, 110, 30]).
+pokemon(ivysaur, grama_venenoso, [venusaur], [60, 62,63,80,80,60]).
+pokemon(venusaur, grama_venenoso, [], [80,82,83,100,100,80]).
 
-pokemon(charmander, fogo, [charmeleon, charizard], [39, 52, 43, 60, 50, 65]).
-pokemon(charmeleon, fogo, [charizard], [39, 52, 43, 60, 50, 65]).
-pokemon(charizard, fogo_voador, [], [39, 52, 43, 60, 50, 65]).
+pokemon(charmander, fogo, [charmeleon, charizard], [39,52,43,60,50,65]).
+pokemon(charmeleon, fogo, [charizard], [58,64,58,80,65,80]).
+pokemon(charizard, fogo_voador, [], [78,84,78,109,85,100]).
 
 pokemon(squirtle, agua, [wartortle, blastoise], [44, 48, 65, 50, 64, 43]).
-pokemon(wartortle, agua, [blastoise], [44, 48, 65, 50, 64, 43]).
-pokemon(blastoise, agua, [], [44, 48, 65, 50, 64, 43]).
+pokemon(wartortle, agua, [blastoise], [59,63,80,65,80,58]).
+pokemon(blastoise, agua, [], [79,83,100,85,105,78]).
 
-pokemon(caterpie, inseto,[metapod, butterfree],[]).
-pokemon(metapod, inseto,[butterfree],[]).
-pokemon(butterfree, inseto_voador,[],[]).
+pokemon(caterpie, inseto,[metapod, butterfree],[45,30,35,20,20,45]).
+pokemon(metapod, inseto,[butterfree],[50,20,55,25,25,30]).
+pokemon(butterfree, inseto_voador,[],[60,45,50,90,80,70]).
 
-pokemon(weedle,inseto_venenoso,[kakuna,beedrill],[]).
-pokemon(kakuna,inseto_venenoso,[beedrill],[]).
-pokemon(beedrill,inseto_venenoso,[],[]).
+pokemon(weedle,inseto_venenoso,[kakuna,beedrill],[40,35,30,20,20,50]).
+pokemon(kakuna,inseto_venenoso,[beedrill],[45,25,50,25,25,35]).
+pokemon(beedrill,inseto_venenoso,[],[65,90,40,45,80,75]).
 
-pokemon(pidgey,normal_voador,[pidgeotto, pidgeot],[]).
-pokemon(pidgeotto,normal_voador,[pidgeot],[]).
-pokemon(pidgeot,normal_voador,[],[]).
+pokemon(pidgey,normal_voador,[pidgeotto, pidgeot],[40,45,40,35,35,56]).
+pokemon(pidgeotto,normal_voador,[pidgeot],[63,60,55,50,50,71]).
+pokemon(pidgeot,normal_voador,[],[83,80,75,70,70,101]).
 
-pokemon(ratata,normal,[raticate],[]).
-pokemon(raticate,normal,[],[]).
+pokemon(ratata,normal,[raticate],[30,56,35,25,35,72]).
+pokemon(raticate,normal,[],[55,81,60,50,70,97]).
 
-pokemon(spearow,normal_voador,[fearow],[]).
-pokemon(fearow,normal_voador,[],[]).
+pokemon(spearow,normal_voador,[fearow],[40,60,30,31,31,70]).
+pokemon(fearow,normal_voador,[],[65,90,65,61,61,100]).
 
-pokemon(ekans,venenoso,[arbok],[]).
-pokemon(arbok,venenoso,[],[]).
+pokemon(ekans,venenoso,[arbok],[35,60,44,40,54,55]).
+pokemon(arbok,venenoso,[],[60,85,69,65,79,80]).
 
-pokemon(pikachu,eletrico,[raichu],[]).
-pokemon(raichu,eletrico,[],[]).
+pokemon(pikachu,eletrico,[raichu],[35,55,40,50,50,90]).
+pokemon(raichu,eletrico,[],[60,90,55,90,80,110]).
 
-pokemon(sandshrew,terrestre,[sandslash],[]).
-pokemon(sandslash,terrestre,[],[]).
+pokemon(sandshrew,terrestre,[sandslash],[50,75,85,20,30,40]).
+pokemon(sandslash,terrestre,[],[75,100,110,45,55,65]).
 
-pokemon(nidoran,venenoso,[nidorina, nidoqueen],[]).
-pokemon(nidorina,venenoso,[nidoqueen],[]).
-pokemon(nidoqueen,venenoso_terrestre,[],[]).
-pokemon(nidoran,venenoso,[nidorina, nidoking],[]).
-pokemon(nidorino,venenoso,[nidoking],[]).
-pokemon(nidoking,venenoso_terrestre,[],[]).
+pokemon(nidoran,venenoso,[nidorina, nidoqueen],[55,47,52,40,40,41]).
+pokemon(nidorina,venenoso,[nidoqueen],[70,62,67,55,55,56]).
+pokemon(nidoqueen,venenoso_terrestre,[],[90,92,87,75,85,76]).
+pokemon(nidoran,venenoso,[nidorina, nidoking],[46,57,40,40,40,50]).
+pokemon(nidorino,venenoso,[nidoking],[61,72,57,55,55,65]).
+pokemon(nidoking,venenoso_terrestre,[],[81,102,77,85,75,85]).
 
-pokemon(clefairy,fada,[clefable],[]).
-pokemon(clefable,fada,[],[]).
+pokemon(clefairy,fada,[clefable],[70,45,48,60,65,35]).
+pokemon(clefable,fada,[],[95,70,73,95,90,60]).
 
-pokemon(vulpix,fogo,[ninetales],[]).
-pokemon(ninetales,fogo,[],[]).
+pokemon(vulpix,fogo,[ninetales],[38,41,40,50,65,65]).
+pokemon(ninetales,fogo,[],[73,79,75,81,100,100]).
 
-pokemon(jigglypuff,normal_fada,[wigglytuff],[]).
-pokemon(wigglytuff,normal_fada,[],[]).
+pokemon(jigglypuff,normal_fada,[wigglytuff],[115,45,20,45,25,20]).
+pokemon(wigglytuff,normal_fada,[],[140,70,45,85,50,45]).
 
-pokemon(zubat,venenoso_voador,[golbat],[]).
-pokemon(golbat,venenoso_voador,[],[]).
+pokemon(zubat,venenoso_voador,[golbat],[40,45,35,30,40,55]).
+pokemon(golbat,venenoso_voador,[],[75,80,70,65,75,90]).
 
-pokemon(oddish,grama_venenoso,[gloom, vileplume],[]).
-pokemon(gloom,grama_venenoso,[vileplume],[]).
-pokemon(vileplume,grama_venenoso,[],[]).
+pokemon(oddish,grama_venenoso,[gloom, vileplume],[45,50,55,75,65,30]).
+pokemon(gloom,grama_venenoso,[vileplume],[60,65,70,85,75,40]).
+pokemon(vileplume,grama_venenoso,[],[75,80,85,110,90,50]).
 
-pokemon(paras,inseto_grama,[parasect],[]).
-pokemon(parasect,inseto_grama,[],[]).
+pokemon(paras,inseto_grama,[parasect],[35,70,55,45,55,25]).
+pokemon(parasect,inseto_grama,[],[60,95,80,60,80,30]).
 
-pokemon(venonat,inseto_venenoso,[venomoth],[]).
-pokemon(venomoth,inseto_venenoso,[],[]).
+pokemon(venonat,inseto_venenoso,[venomoth],[60,55,50,40,55,45]).
+pokemon(venomoth,inseto_venenoso,[],[70,65,60,90,75,90]).
 
-pokemon(diglett,terrestre,[dugtrio],[]).
-pokemon(dugtrio,terrestre,[],[]).
+pokemon(diglett,terrestre,[dugtrio],[10,55,25,35,45,95]).
+pokemon(dugtrio,terrestre,[],[35,80,50,50,70,120]).
 
-pokemon(meowth,normal,[persian],[]).
-pokemon(persian,normal,[],[]).
+pokemon(meowth,normal,[persian],[40,45,35,40,40,90]).
+pokemon(persian,normal,[],[65,70,60,65,65,115]).
 
-pokemon(psyduck,agua,[golduck],[]).
-pokemon(golduck,agua,[],[]).
+pokemon(psyduck,agua,[golduck],[50,52,48,65,50,55]).
+pokemon(golduck,agua,[],[80,82,78,95,80,85]).
 
-pokemon(mankey,lutador,[primeape],[]).
-pokemon(primeape,lutador,[],[]).
+pokemon(mankey,lutador,[primeape],[40,80,35,35,45,70]).
+pokemon(primeape,lutador,[],[65,105,60,60,70,95]).
 
-pokemon(growlithe,fogo,[arcanine],[]).
-pokemon(arcanine,fogo,[],[]).
+pokemon(growlithe,fogo,[arcanine],[55,70,45,70,50,60]).
+pokemon(arcanine,fogo,[],[90,110,80,100,80,95]).
 
-pokemon(poliwag,agua,[poliwhirl, poliwrath],[]).
-pokemon(poliwhirl,agua,[poliwrath],[]).
-pokemon(poliwrath,agua,[],[]).
+pokemon(poliwag,agua,[poliwhirl, poliwrath],[40,50,40,40,40,90]).
+pokemon(poliwhirl,agua,[poliwrath],[65,65,65,50,50,90]).
+pokemon(poliwrath,agua,[],[90,95,95,70,90,70]).
 
-pokemon(abra,psiquico,[kadabra, alakazam],[]).
-pokemon(kadabra,psiquico,[alakazam],[]).
-pokemon(alakazam,psiquico,[],[]).
+pokemon(abra,psiquico,[kadabra, alakazam],[25,20,15,105,55,90]).
+pokemon(kadabra,psiquico,[alakazam],[40,35,30,120,70,105]).
+pokemon(alakazam,psiquico,[],[55,50,45,135,95,120]).
 
-pokemon(machop,lutador,[machoke, machamp],[]).
-pokemon(machoke,lutador,[machamp],[]).
-pokemon(machamp,lutador,[],[]).
+pokemon(machop,lutador,[machoke, machamp],[70,80,50,35,35,35]).
+pokemon(machoke,lutador,[machamp],[80,100,70,50,60,45]).
+pokemon(machamp,lutador,[],[90,130,80,65,85,55]).
 
-pokemon(bellsprout,grama_venenoso,[weepinbell, victreebel],[]).
-pokemon(weepinbell,grama_venenoso,[victreebel],[]).
-pokemon(victreebel,grama_venenoso,[],[]).
+pokemon(bellsprout,grama_venenoso,[weepinbell, victreebel],[50,75,35,70,30,40]).
+pokemon(weepinbell,grama_venenoso,[victreebel],[65,90,50,85,45,55]).
+pokemon(victreebel,grama_venenoso,[],[80,105,65,100,70,70]).
 
-pokemon(tentacool,agua_venenoso,[tentacruel],[]).
-pokemon(tentacruel,agua_venenoso,[],[]).
+pokemon(tentacool,agua_venenoso,[tentacruel],[40,40,35,50,100,70]).
+pokemon(tentacruel,agua_venenoso,[],[80,70,65,80,120,100]).
 
-pokemon(geodude,pedra_terrestre,[graveler, golem],[]).
-pokemon(graveler,pedra_terrestre,[golem],[]).
-pokemon(golem,pedra_terrestre,[],[]).
+pokemon(geodude,pedra_terrestre,[graveler, golem],[40,80,100,30,30,20]).
+pokemon(graveler,pedra_terrestre,[golem],[55,95,115,45,45,35]).
+pokemon(golem,pedra_terrestre,[],[80,120,130,55,65,45]).
 
-pokemon(ponyta,fogo,[rapidash],[]).
-pokemon(rapidash,fogo,[],[]).
+pokemon(ponyta,fogo,[rapidash],[50,85,55,65,65,90]).
+pokemon(rapidash,fogo,[],[65,100,70,80,80,105]).
 
-pokemon(slowpoke,agua_psiquico,[slowbro],[]).
-pokemon(slowbro,agua_psiquico,[],[]).
+pokemon(slowpoke,agua_psiquico,[slowbro],[90,65,65,40,40,15]).
+pokemon(slowbro,agua_psiquico,[],[95,75,110,100,80,30]).
 
-pokemon(magnemite,eletrico_aco,[magneton],[]).
-pokemon(magneton,eletrico_aco,[],[]).
+pokemon(magnemite,eletrico_aco,[magneton],[25,35,70,95,55,45]).
+pokemon(magneton,eletrico_aco,[],[50,60,95,120,70,70]).
 
-pokemon(farfetchd,normal_voador,[],[]).
+pokemon(farfetchd,normal_voador,[],[52,65,55,58,62,60]).
 
-pokemon(doduo,normal_voador,[dodrio],[]).
-pokemon(dodrio,normal_voador,[],[]).
+pokemon(doduo,normal_voador,[dodrio],[35,85,45,35,35,75]).
+pokemon(dodrio,normal_voador,[],[60,110,70,60,60,100]).
 
-pokemon(seel,agua,[dewgong],[]).
-pokemon(dewgong,agua,[],[]).
+pokemon(seel,agua,[dewgong],[65,45,55,45,70,45]).
+pokemon(dewgong,agua,[],[90,70,80,70,95,70]).
 
-pokemon(grimer,venenoso,[muk],[]).
-pokemon(muk,venenoso,[],[]).
+pokemon(grimer,venenoso,[muk],[80,80,50,40,50,25]).
+pokemon(muk,venenoso,[],[105,105,75,65,100,50]).
 
-pokemon(shellder,agua,[cloyster],[]).
-pokemon(cloyster,agua_gelo,[],[]).
+pokemon(shellder,agua,[cloyster],[30,65,100,45,25,40]).
+pokemon(cloyster,agua_gelo,[],[50,95,180,85,45,70]).
 
-pokemon(gastly,fantasma_venenoso,[haunter, gengar],[]).
-pokemon(haunter, fantasma_venenoso,[gengar],[]).
-pokemon(gengar,fantasma_venenoso,[],[]).
+pokemon(gastly,fantasma_venenoso,[haunter, gengar],[30,35,30,100,35,80]).
+pokemon(haunter, fantasma_venenoso,[gengar],[45,50,45,115,55,95]).
+pokemon(gengar,fantasma_venenoso,[],[60,65,60,130,75,110]).
 
-pokemon(onix,pedra_terrestre,[],[]).
+pokemon(onix,pedra_terrestre,[],[35,45,160,30,45,70]).
 
-pokemon(drowzee,psiquico,[hypno],[]).
-pokemon(hypno,psiquico,[],[]).
+pokemon(drowzee,psiquico,[hypno],[60,48,45,43,90,42]).
+pokemon(hypno,psiquico,[],[85,73,70,73,115,67]).
 
-pokemon(krabby,agua,[kingler],[]).
-pokemon(kingler,agua,[],[]).
+pokemon(krabby,agua,[kingler],[30,105,90,25,25,50]).
+pokemon(kingler,agua,[],[50,130,115,50,50,75]).
 
-pokemon(voltorb,eletrico,[electrode],[]).
-pokemon(electrode,eletrico,[],[]).
+pokemon(voltorb,eletrico,[electrode],[40,30,50,55,55,100]).
+pokemon(electrode,eletrico,[],[60,50,70,80,80,140]).
 
-pokemon(exeggcute,grama_psiquico,[exeggutor],[]).
-pokemon(exeggutor,grama_psiquico,[],[]).
+pokemon(exeggcute,grama_psiquico,[exeggutor],[60,40,80,60,45,40]).
+pokemon(exeggutor,grama_psiquico,[],[95,95,85,125,65,55]).
 
-pokemon(cubone,terrestre,[marowak],[]).
-pokemon(marowak,terrestre,[],[]).
+pokemon(cubone,terrestre,[marowak],[50,50,95,40,50,35]).
+pokemon(marowak,terrestre,[],[60,80,110,50,80,45]).
 
-pokemon(hitmonlee,lutador,[],[]).
-pokemon(hitmonchan,lutador,[],[]).
+pokemon(hitmonlee,lutador,[],[50,120,53,35,110,87]).
+pokemon(hitmonchan,lutador,[],[50,105,79,35,110,76]).
 
-pokemon(lickitung,normal,[],[]).
+pokemon(lickitung,normal,[],[90,55,75,60,75,30]).
 
-pokemon(coffing,venenoso,[weezing],[]).
-pokemon(weezing,venenoso,[],[]).
+pokemon(coffing,venenoso,[weezing],[40,65,95,60,45,35]).
+pokemon(weezing,venenoso,[],[65,90,120,85,70,60]).
 
-pokemon(rhyhorn,pedra_terrestre,[rhydon],[]).
-pokemon(rhydon,pedra_terrestre,[],[]).
+pokemon(rhyhorn,pedra_terrestre,[rhydon],[80,85,95,30,30,25]).
+pokemon(rhydon,pedra_terrestre,[],[105,130,120,45,45,40]).
 
-pokemon(chansey, normal,[],[]).
-pokemon(tangela,grama,[],[]).
-pokemon(kangaskhan,normal,[],[]).
+pokemon(chansey, normal,[],[250,5,5,35,105,50]).
+pokemon(tangela,grama,[],[65,55,115,100,40,60]).
+pokemon(kangaskhan,normal,[],[105,95,80,40,80,90]).
 
-pokemon(horsea,agua,[seadra],[]).
-pokemon(seadra,agua,[],[]).
+pokemon(horsea,agua,[seadra],[30,40,70,70,25,60]).
+pokemon(seadra,agua,[],[55,65,95,95,45,85]).
 
-pokemon(goldean,agua,[seaking],[]).
-pokemon(seaking,agua,[],[]).
+pokemon(goldean,agua,[seaking],[45,67,60,35,50,63]).
+pokemon(seaking,agua,[],[80,92,65,65,80,68]).
 
-pokemon(staryu,agua,[starmie],[]).
-pokemon(starmie,agua,[],[]).
+pokemon(staryu,agua,[starmie],[30,45,55,70,55,85]).
+pokemon(starmie,agua,[],[60,75,85,100,85,115]).
 
-pokemon(mr_mime,psiquico_fada,[],[]).
+pokemon(mr_mime,psiquico_fada,[],[40,45,65,100,120,90]).
 
-pokemon(scyther,inseto_voador,[],[]).
-pokemon(jynx,gelo_psiquico,[],[]).
+pokemon(scyther,inseto_voador,[],[70,110,80,55,80,105]).
+pokemon(jynx,gelo_psiquico,[],[65,50,35,115,95,95]).
 
-pokemon(electabuzz,eletrico,[],[]).
-pokemon(magmar,fogo,[],[]).
+pokemon(electabuzz,eletrico,[],[65,83,57,95,85,105]).
+pokemon(magmar,fogo,[],[65,95,57,100,85,93]).
 
-pokemon(pinsir,inseto,[],[]).
+pokemon(pinsir,inseto,[],[65,125,100,55,70,85]).
 
-pokemon(tauros,normal,[],[]).
+pokemon(tauros,normal,[],[75,100,95,40,70,110]).
 
-pokemon(magikarp,agua,[gyarados],[]).
-pokemon(gyarados,agua_voador,[],[]).
+pokemon(magikarp,agua,[gyarados],[20,10,55,15,20,80]).
+pokemon(gyarados,agua_voador,[],[95,125,79,60,100,81]).
 
-pokemon(lapras,agua_gelo,[],[]).
-pokemon(ditto,normal,[],[]).
+pokemon(lapras,agua_gelo,[],[130,85,80,85,95,60]).
+pokemon(ditto,normal,[],[48,48,48,48,48,48]).
 
-pokemon(eevee,normal,[vaporeon, jolteon, flareon],[]).
-pokemon(vaporeon,agua,[],[]).
-pokemon(jolteon,eletrico,[],[]).
-pokemon(flareon,fogo,[],[]).
+pokemon(eevee,normal,[vaporeon, jolteon, flareon],[55,55,50,45,65,55]).
+pokemon(vaporeon,agua,[],[130,65,60,110,95,65]).
+pokemon(jolteon,eletrico,[],[65,65,60,110,95,130]).
+pokemon(flareon,fogo,[],[65,130,60,95,110,65]).
 
-pokemon(porygon,normal,[],[]).
+pokemon(porygon,normal,[],[65,60,70,85,75,40]).
 
-pokemon(omanyte,pedra_agua,[omastar],[]).
-pokemon(omastar,pedra_agua,[],[]).
+pokemon(omanyte,pedra_agua,[omastar],[35,40,100,90,55,35]).
+pokemon(omastar,pedra_agua,[],[70,60,125,115,70,55]).
 
-pokemon(kabuto,pedra_agua,[kabutops],[]).
-pokemon(kabutops,pedra_agua,[],[]).
+pokemon(kabuto,pedra_agua,[kabutops],[30,80,90,55,45,55]).
+pokemon(kabutops,pedra_agua,[],[60,115,105,65,70,80]).
 
-pokemon(aerodactyl,pedra_voador,[],[]).
+pokemon(aerodactyl,pedra_voador,[],[80,105,65,60,75,130]).
 
-pokemon(snorlax,normal,[],[]).
+pokemon(snorlax,normal,[],[160,110,65,65,110,30]).
 
-pokemon(articuno,gelo_voador,[],[]).
-pokemon(zapdos,eletrico_voador,[],[]).
-pokemon(moltres,fogo_voador,[],[]).
+pokemon(articuno,gelo_voador,[],[90,85,100,95,125,85]).
+pokemon(zapdos,eletrico_voador,[],[90,90,85,125,90,100]).
+pokemon(moltres,fogo_voador,[],[90,100,90,125,85,90]).
 
-pokemon(dratini,dragon,[dragonair, dragonite],[]).
-pokemon(dragonair,dragon,[dragonite],[]).
-pokemon(dragonite,dragon_voador,[],[]).
+pokemon(dratini,dragon,[dragonair, dragonite],[41,64,45,50,50,50]).
+pokemon(dragonair,dragon,[dragonite],[61,84,65,70,70,70]).
+pokemon(dragonite,dragon_voador,[],[91,134,95,100,100,80]).
 
-pokemon(mewtwo,psiquico,[],[]).
-pokemon(mew,psiquico,[],[]).
+pokemon(mewtwo,psiquico,[],[106,110,90,154,90,130]).
+pokemon(mew,psiquico,[],[100,100,100,100,100,100]).
 
 
 
