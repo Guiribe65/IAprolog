2e7b8d62229c13aef031031b40ba276627fd54bd
Pedro <pedrohda.ph@gmail.com>
2023-11-23 19:49:48 -0300

    Telas de inicio e fim

---
+++ Projeto3D/Assets/Scripts/ComecarJogo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ComecarJogo : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+
+        SceneManager.LoadScene("Fase1");
+
+
+    }
+    public void button_exit()
+    {
+
+        Application.Quit();
+
+    }
+}
