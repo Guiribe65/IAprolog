bbe239de35766b5a16f386ad913f111cfc28ede0
Gui <guiribe65@gmail.com>
2023-11-22 21:50:52 -0300

    coisas novas

---
+++ Projeto3D/Assets/Scripts/Atira.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atira : MonoBehaviour
+{
+    public Transform kunai;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 10, Input.GetAxis("Vertical"));
+
+        if (dir != Vector3.zero)
+        {
+            transform.forward = Vector3.Normalize(dir);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Obter a rotação atual da câmera
+            Quaternion cameraRotation = Camera.main.transform.rotation;
+
+            // Instanciar o kunai com a rotação da câmera
+            Instantiate(kunai, transform.position, cameraRotation);
+        }
+    }
+}
---
+++ Projeto3D/Assets/Scripts/tiro.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class tiro : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            rb = gameObject.AddComponent<Rigidbody>();
+        }
+
+        if (rb != null)
+        {
+            Vector3 cameraForward = Camera.main.transform.forward;
+            rb.AddForce(cameraForward * 2000);
+        }
+        else
+        {
+            Debug.LogError("Rigidbody não encontrado ou adicionado a este objeto!");
+        }
+    }
+
+    void Update()
+    {
+        Destroy(gameObject, 1f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Inimigo"))
+        {
+            Inimigo inimigoScript = other.GetComponent<Inimigo>();
+
+            // Parar o inimigo definindo a posição do destino como sua posição atual
+            if (inimigoScript != null)
+            {
+                inimigoScript.PararInimigo();
+            }
+
+            // Agora você pode destruir o objeto inimigo
+            Destroy(other.gameObject);
+
+            // Destruir o projetil
+            Destroy(gameObject);
+        }
+    }
+}
